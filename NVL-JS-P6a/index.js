01562579b88eb6aa26ed414d02a01ab68f8f7d35
pedri77 <dmgmoya85@gmail.com>
2018-12-31 08:32:01 +0100

    NVL-JS-P6a

---
+++ NVL-JS-P6a/index.js
@@ -0,0 +1,12 @@
+// un nombre y apellidos
+// Tu edad
+// Los días de la semana
+// El valor de la gravedad
+// Los 3 primeros números impares
+
+var nombre_apellidos = Juan Gutierrez Campos
+var edad = 41
+var dias_semama = [lunes, martes, miercoles, jueves, viernes, sabado, domingo]
+var gavedad = 9.8
+var Primeros_tres_impares = [1,3,9]
+
